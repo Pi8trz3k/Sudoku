61e8c295a5f1bd094cb8034ab27f8e00b6900144
Kacper Pietrzak <242495@edu.p.lodz.pl>
2022-10-21 20:01:14 +0200

    Implementacja klasy, przeniesienie i refaktoryzacja metod z SudokuBoard

--- src/main/java/org/example/BacktrackingSudokuSolver.java
+++ src/main/java/org/example/BacktrackingSudokuSolver.java
@@ -1,5 +1,123 @@
 package org.example;
 
-public class BacktrackingSudokuSolver {
+import java.util.List;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class BacktrackingSudokuSolver implements SudokuSolver {
+
+    public void solveGame(SudokuBoard board) {
+        solve(board);
+    }
+    @Override
+    public void solve(SudokuBoard board) {
+        fillBoard(board);
+    }
+
+    public static void shuffleFirstRow(SudokuBoard board) {
+        Integer[] firstRow = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        List<Integer> firstRowList = Arrays.asList(firstRow);
+        Collections.shuffle(firstRowList);
+        firstRowList.toArray(firstRow);
+        for(int i = 0; i < 9; i++) {
+            board.set(i,0,firstRow[i]); //[i][0] = firstRow[i];
+        }
+    }
+
+    public void fillBoard(SudokuBoard board) {
+        shuffleFirstRow(board);
+        solveBoard(board, 1);
+    }
+
+    public static boolean solveBoard(SudokuBoard board, int number) {
+        if (isEnd(board)) {
+            return true;
+        }
+        if (findAllRepeats(board, number) >= 9) {
+            number++;
+        }
+        int[][] points = new int[81][2];
+        int numberOfPoints = findAllPossiblePoints(board, number, points);
+        for (int i = 0; i < numberOfPoints; i++) {
+            fillPoint(board, points[i][0], points[i][1], number);
+            if (solveBoard(board, number)) {
+                return true;
+            } else {
+                unFillPoint(board, points[i][0], points[i][1]);
+            }
+        }
+        return false;
+    }
+
+    public static boolean isEnd(SudokuBoard board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board.get(i,j) == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static int findAllRepeats(SudokuBoard board, int number) {
+        int repeats = 0;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board.get(i,j) == number) {
+                    repeats++;
+                }
+            }
+        }
+        return repeats;
+    }
+
+    public static int findAllPossiblePoints(SudokuBoard board, int number, int[][] points) {
+        int numberOfPoints = 0;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (isCorrect(board, i, j, number)) {
+                    points[numberOfPoints][0] = i;
+                    points[numberOfPoints][1] = j;
+                    numberOfPoints++;
+                }
+            }
+        }
+        return numberOfPoints;
+    }
+
+    public static void fillPoint(SudokuBoard board, int x, int y, int number) {
+        board.set(x,y,number);//[x][y] = number;
+    }
+
+    public static void unFillPoint(SudokuBoard board, int x, int y) {
+        board.set(x,y,0);//[x][y] = 0;
+    }
+
+    public static boolean isCorrect(SudokuBoard board, int x, int y, int number) {
+
+        if (board.get(x,y) != 0) {
+            return false;
+        }
+
+        for (int i = 0; i < 9; i++) {
+            if (board.get(i,y) == number || board.get(x,i) == number) {
+                return false;
+            }
+        }
+
+        return !usedInBox(board, x - (x % 3), y - (y % 3), number);
+    }
+
+    public static boolean usedInBox(SudokuBoard board, int startRow, int startCol, int number) {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (board.get(row + startRow,col + startCol) == number) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
 }
