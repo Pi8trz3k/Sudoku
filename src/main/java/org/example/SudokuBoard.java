a0f2cf8a4ce3e3548cd10958c3ed10916ba9efae
242456 Łaski Robert <242456@edu.p.lodz.pl>
2022-10-21 16:30:17 +0200

    Refactoring klasy SudokuSolver, oddzielenie algorytmu rozwiązywania sudoku od klasy, stworzenie nowych klas

--- src/main/java/org/example/SudokuBoard.java
+++ src/main/java/org/example/SudokuBoard.java
@@ -1,118 +1,7 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class SudokuBoard {
     private int[][] board = new int[9][9];
-
-    public static void shuffleFirstRow(int[][] board) {
-        Integer[] firstRow = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        List<Integer> firstRowList = Arrays.asList(firstRow);
-        Collections.shuffle(firstRowList);
-        firstRowList.toArray(firstRow);
-        for (int i = 0; i < 9; i++) {
-            board[i][0] = firstRow[i];
-        }
-    }
-
-    public void fillBoard() {
-        shuffleFirstRow(this.board);
-        solveBoard(this.board, 0, 1, 1);
-    }
-
-    public static boolean solveBoard(int[][] board, int x, int y, int number) {
-        if (isEnd(board)) {
-            return true;
-        }
-        if (findAllRepeats(board, number) >= 9) {
-            number++;
-        }
-        int[][] points = new int[81][2];
-        int numberOfPoints = findAllPossiblePoints(board, number, points);
-        for (int i = 0; i < numberOfPoints; i++) {
-            fillPoint(board, points[i][0], points[i][1], number);
-            if (solveBoard(board, points[i][0], points[i][1], number)) {
-                return true;
-            } else {
-                unFillPoint(board, points[i][0], points[i][1]);
-            }
-        }
-        return false;
-    }
-
-    public static boolean isEnd(int[][] board) {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] == 0) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    public static int findAllRepeats(int[][] board, int number) {
-        int repeats = 0;
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] == number) {
-                    repeats++;
-                }
-            }
-        }
-        return repeats;
-    }
-
-    public static int findAllPossiblePoints(int[][] board, int number, int[][] points) {
-        int numberOfPoints = 0;
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (isCorrect(board, i, j, number)) {
-                    points[numberOfPoints][0] = i;
-                    points[numberOfPoints][1] = j;
-                    numberOfPoints++;
-                }
-            }
-        }
-        return numberOfPoints;
-    }
-
-    public static void fillPoint(int[][] board, int x, int y, int number) {
-        board[x][y] = number;
-    }
-
-    public static void unFillPoint(int[][] board, int x, int y) {
-        board[x][y] = 0;
-    }
-
-    public static boolean isCorrect(int[][] board, int x, int y, int number) {
-
-        if (board[x][y] != 0) {
-            return false;
-        }
-
-        for (int i = 0; i < 9; i++) {
-            if (board[i][y] == number || board[x][i] == number) {
-                return false;
-            }
-        }
-
-        return !usedInBox(board, x - (x % 3), y - (y % 3), number);
-    }
-
-    public static boolean usedInBox(int[][] board, int startRow, int startCol, int number) {
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                if (board[row + startRow][col + startCol] == number) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
 
     public void printBoard() {
         for (int i = 0; i < 9; i++) {
@@ -123,16 +12,53 @@ public class SudokuBoard {
         }
     }
 
-    public void setBoard(int[][] board) {
-        this.board = board;
+    public void setBoard(int x, int y, int number) {
+        this.board[x][y] = number;
     }
 
-    public int[][] getBoard() {
-        int[][] sudokuBoard = new int[9][9];
+    public int get(int x, int y) {
+        return this.board[x][y];
+    }
+
+
+    public boolean checkBoard() {
+        //vertical test
         for (int i = 0; i < 9; i++) {
-            System.arraycopy(board[i], 0, sudokuBoard[i], 0, 9);
+            for (int j = 0; j < 9; j++) {
+                for (int jj = j + 1; jj < 9; jj++) {
+                    if (board[i][j] == board[i][jj]) {
+                        return false;
+                    }
+                }
+            }
         }
-        return sudokuBoard;
+        //horizontal test
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                for (int jj = j + 1; jj < 9; jj++) {
+                    if (board[j][i] == board[jj][i]) {
+                        return false;
+                    }
+                }
+            }
+        }
+        //square test
+        int powt = 0;
+        for (int a = 1; a < 10; a++) {
+            for (int z = 0; z < 9; z++) {
+                for (int i = z % 3 * 3; i < z % 3 * 3 + 3; i++) {
+                    for (int j = z / 3 * 3; j < z / 3 * 3 + 3; j++) {
+                        if (board[i][j] == a) {
+                            powt++;
+                        }
+                    }
+                }
+                if (powt > 1) {
+                    return false;
+                }
+                powt = 0;
+            }
+        }
+        return true;
     }
-
 }
