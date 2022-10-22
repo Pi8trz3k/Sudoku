292ec36bcd81b74bec5686455f2b0c2ada31faaa
Kacper Pietrzak <242495@edu.p.lodz.pl>
2022-10-22 19:15:38 +0200

    Refaktoryzacja testow w klasie BacktracingSudokuSolverTest

--- src/test/java/org/example/BacktrackingSudokuSolverTest.java
+++ src/test/java/org/example/BacktrackingSudokuSolverTest.java
@@ -4,64 +4,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 class BacktrackingSudokuSolverTest {
-    private boolean checkVertical(SudokuBoard board){
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                for (int jj = j + 1; jj < 9; jj++) {
-                    if (board.get(i, j) == board.get(i, jj)) {
-                        return false;
-                    }
-                }
-            }
-        }
-        return true;
-    }
-    private boolean checkHorizontal(SudokuBoard board){
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                for (int jj = j + 1; jj < 9; jj++) {
-                    if (board.get(j,i) == board.get(jj, i)) {
-                        return false;
-                    }
-                }
-            }
-        }
-        return true;
-    }
 
-    private boolean checkBox(SudokuBoard board){
-        int powt = 0;
-        for (int a = 1; a < 10; a++) {
-            for (int z = 0; z < 9; z++) {
-                for (int i = z % 3 * 3; i < z % 3 * 3 + 3; i++) {
-                    for (int j = z / 3 * 3; j < z / 3 * 3 + 3; j++) {
-                        if (board.get(i, j) == a) {
-                            powt++;
-                        }
-                    }
-                }
-                if (powt > 1) {
-                    return false;
-                }
-                powt = 0;
-            }
-        }
-        return true;
-    }
-
-
+    private final SudokuBoard sudoku = new SudokuBoard();
     @Test
     void solveTest() {
-        SudokuBoard sudoku = new SudokuBoard();
         sudoku.solveGame();
-        Assertions.assertTrue(checkHorizontal(sudoku));
-        Assertions.assertTrue(checkVertical(sudoku));
-        Assertions.assertTrue(checkBox(sudoku));
+        Assertions.assertTrue(sudoku.checkHorizontal());
+        Assertions.assertTrue(sudoku.checkVertical());
+        Assertions.assertTrue(sudoku.checkSquare());
     }
 
     @Test
     void solveRepeatTest() {
-        SudokuBoard sudoku = new SudokuBoard();
         SudokuBoard sudoku2 = new SudokuBoard();
         sudoku.solveGame();
         sudoku2.solveGame();
