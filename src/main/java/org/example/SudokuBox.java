8b761ba42f9d651847491e74fe67091429f2e9fa
242456 Łaski Robert <242456@edu.p.lodz.pl>
2022-11-04 14:59:49 +0100

    Dodanie klasy SudokuFieldType, SudokuBox, SudokuRow i SudokuColumn

---
+++ src/main/java/org/example/SudokuBox.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class SudokuBox extends SudokuFieldType {
+    public static final int box_size = 3;
+
+    public SudokuBox(final SudokuField[] fields) {
+        super(fields);
+    }
+}
---
+++ src/main/java/org/example/SudokuFieldType.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class SudokuFieldType {
+    public static final int size = 9;
+    protected SudokuField[] fields;
+
+    public SudokuFieldType(final SudokuField[] fields) {
+        if (fields.length == size) {
+            this.fields = fields;
+        }
+    }
+
+    public boolean verify() {
+        for (int i = 0; i < 9; i++) {
+            for (int i2 = i + 1; i2 < 9; i2++) {
+                if (fields[i].getFieldValue() == fields[i2].getFieldValue()) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+
+}
