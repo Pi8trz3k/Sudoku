a972a5cde5a5fa482adb6ed43fc401d8dfcab5a5
242456 Łaski Robert <242456@edu.p.lodz.pl>
2022-11-04 14:45:58 +0100

    Dodanie klasy SudokuField

---
+++ src/main/java/org/example/SudokuField.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public abstract class SudokuField {
+    private int value;
+
+    public SudokuField() {
+
+    }
+
+    public SudokuField(int value) {
+        this.value = value;
+    }
+
+    public int getFieldValue() {
+        return this.value;
+    }
+
+    public void setFieldValue(int value) {
+        if (value > 0 && value < 10) {
+            this.value = value;
+        }
+    }
+}
