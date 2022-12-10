1791a493dcf13df8fd5e44787bf62f6398306466
Kacper Pietrzak <242495@edu.p.lodz.pl>
2022-12-10 18:35:16 +0100

    Implementacja interefejsu Cloneable SudokuBox

--- Model/src/main/java/org/example/SudokuBox.java
+++ Model/src/main/java/org/example/SudokuBox.java
@@ -1,11 +1,25 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class SudokuBox extends SudokuFieldType {
+public class SudokuBox extends SudokuFieldType implements Cloneable {
     public static final int box_size = 3;
 
     public SudokuBox(List<SudokuField> fields) {
         super(fields);
+    }
+
+    @Override
+    public SudokuBox clone() {
+        try {
+            SudokuBox result = (SudokuBox) super.clone();
+            for (int i = 0; i < size; i++) {
+                result.fields.set(i, fields.get(i));
+            }
+            return result;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
     }
 }
