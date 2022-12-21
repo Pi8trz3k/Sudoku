4b94848445158e042b1dc29479e62c377634f1de
Kacper Pietrzak <242495@edu.p.lodz.pl>
2022-12-21 20:11:06 +0100

    Dodanie klasy Messages

---
+++ Model/src/main/java/org/example/Messages.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Messages {
+    public static String getMessage(String message, Locale locale) {
+        return ResourceBundle.getBundle("messages", locale).getString(message);
+    }
+}
