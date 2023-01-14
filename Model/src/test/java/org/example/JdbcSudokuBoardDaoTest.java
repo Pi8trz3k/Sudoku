2185a0c9cce8e71f7792954d094915ee2f3c46d2
Kacper Pietrzak <242495@edu.p.lodz.pl>
2023-01-14 16:45:03 +0100

    Dodanie testu komponentu JdbcSudokuBoardDao

---
+++ Model/src/test/java/org/example/JdbcSudokuBoardDaoTest.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JdbcSudokuBoardDaoTest {
+
+    @Test
+    public void test1() {
+        String name = "nazwaTestowa";
+        BacktrackingSudokuSolver solver = new BacktrackingSudokuSolver();
+        SudokuBoard sb = new SudokuBoard(solver);
+        sb.solveGame();
+        SudokuBoard newB;
+
+        try (JdbcSudokuBoardDao jdbcTest = new JdbcSudokuBoardDao(name)) {
+            jdbcTest.write(sb);
+
+            newB = jdbcTest.read();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        assertEquals(sb, newB);
+    }
+}
